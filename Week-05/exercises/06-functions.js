1e592a4d5885bd3d4b5e18f4174a2c71d282a68d
Juan Sobral <JuanignacioSobral@gmail.com>
2023-04-17 01:48:30 -0300

    BaSP-W05-2023: Finished up to excercise 6

--- Week-05/exercises/06-functions.js
+++ Week-05/exercises/06-functions.js
@@ -13,17 +13,68 @@ var num2 = 9;
 
 console.log(suma(num2, num1));
 
-//Copiar y renombrar la función suma del ejercicio 6b) y agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alert con el error y retornar el número convertido a entero (redondeado).
+//Copiar la función suma anterior y agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar un alert aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
 console.log("Excercise 6.b");
 
+function suma (a, b){
+    if (isNaN(a)||isNaN(b)){
+        alert("Uno de los parámetros no es un numero");
+        return "NaN";
+    }else {
+        return (a+b);
+    }
+}
 
 //Crear una función “validateInteger” que reciba un número como parámetro y devuelva verdadero si es un número entero.
 console.log("Excercise 6.c");
 
+function validateInteger (numeroIncognita) {
+    if (Math.round(numeroIncognita) === numeroIncognita){
+        return true;
+    } else {
+        return false;
+    }
 
-//Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de una nueva función probando que todo siga funcionando igual que en el apartado anterior.
+}
+
+//Copiar y renombrar la función suma del ejercicio 6b) y agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alert con el error y retornar el número convertido a entero (redondeado).
 console.log("Excercise 6.d");
 
+function llamada (a, b){
+    if (isNaN(a)||isNaN(b)){
+        alert("Uno de los parámetros no es un numero");
+        return "NaN";
+    }   else if ((validateInteger(a)) && !(validateInteger(b))) {
+        alert(b+" is not a round number, please put "+Math.round(b)+" instead");
+    }   else if (!(validateInteger(a)) && (validateInteger(b))) {
+        alert(a+" is not a round number, please put "+Math.round(a)+" instead");
+    }   else if (!(validateInteger(a)) && !(validateInteger(b))) {
+        alert("Neither "+a+" nor "+b+" are round numbers, please put "+Math.round(a)+" and "+Math.round(b)+" instead");
+    }   else if (validateInteger(a) && validateInteger(b)){
+        return (a+b);
+    }
+}
 
-//Copiar la función suma anterior y agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar un alert aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+//Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de una nueva función probando que todo siga funcionando igual que en el apartado anterior.
 console.log("Excercise 6.e");
+
+function roundValidation (a, b){
+    if ((validateInteger(a)) && !(validateInteger(b))) {
+        alert(b+" is not a round number, please put "+Math.round(b)+" instead");
+    }   else if (!(validateInteger(a)) && (validateInteger(b))) {
+        alert(a+" is not a round number, please put "+Math.round(a)+" instead");
+    }   else if (!(validateInteger(a)) && !(validateInteger(b))) {
+        alert("Neither "+a+" nor "+b+" are round numbers, please put "+Math.round(a)+" and "+Math.round(b)+" instead");
+    }   else if (validateInteger(a) && validateInteger(b)){
+        return (a+b);
+    }
+}
+
+function check (a, b){
+    if (isNaN(a)||isNaN(b)){
+        alert("Uno de los parámetros no es un numero");
+        return "NaN";
+    } else {
+        roundValidation(a, b);
+    }
+}
