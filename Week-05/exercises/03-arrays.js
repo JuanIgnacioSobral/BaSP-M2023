0857f236ce4f64a656b000825bee8b182768e4a3
Juan Sobral <JuanignacioSobral@gmail.com>
2023-04-16 22:43:28 -0300

    BaSP-W05-2023: Finished up to excersice 5

--- Week-05/exercises/03-arrays.js
+++ Week-05/exercises/03-arrays.js
@@ -1,1 +1,51 @@
 console.log("EXCERCISE 3: ARRAYS");
+
+// 3.a: Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).
+console.log("Excercise 3.a");
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses[5]);
+console.log(meses[11]);
+
+
+// 3. b:Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+console.log("Excercise 3.b");
+
+meses.sort();
+console.log(meses);
+
+// 3. c:Agregar un elemento al principio y al final del array (utilizar unshift y push).
+console.log("Excercise 3.c");
+
+meses.unshift("Principio");
+meses.push("Final");
+
+console.log(meses);
+
+// 3. d:Quitar un elemento del principio y del final del array (utilizar shift y pop).
+console.log("Excercise 3.d");
+
+meses.shift("Principio");
+meses.pop("Final");
+
+console.log(meses);
+
+// 3. e:Invertir el orden del array (utilizar reverse).
+console.log("Excercise 3.e");
+
+meses.reverse();
+
+console.log(meses);
+
+// 3. f: Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+console.log("Excercise 3.f");
+
+console.log(meses.join("-"));
+
+// 3. g:Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+console.log("Excercise 3.g");
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses.slice(4, -1));
---
+++ Week-05/exercises/06-functions.js
@@ -0,0 +1,29 @@
+console.log("EXCERCISE 6: FUNCTIONS");
+
+
+//Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+console.log("Excercise 6.a");
+
+function suma (a, b){
+    return (a+b);
+}
+
+var num1 = 60;
+var num2 = 9;
+
+console.log(suma(num2, num1));
+
+//Copiar y renombrar la función suma del ejercicio 6b) y agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alert con el error y retornar el número convertido a entero (redondeado).
+console.log("Excercise 6.b");
+
+
+//Crear una función “validateInteger” que reciba un número como parámetro y devuelva verdadero si es un número entero.
+console.log("Excercise 6.c");
+
+
+//Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de una nueva función probando que todo siga funcionando igual que en el apartado anterior.
+console.log("Excercise 6.d");
+
+
+//Copiar la función suma anterior y agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar un alert aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+console.log("Excercise 6.e");
