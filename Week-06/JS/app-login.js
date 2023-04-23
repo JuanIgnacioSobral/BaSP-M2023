cc063bf622e0f050b3eadd1a9c347597f0deccf1
Juan Sobral <JuanignacioSobral@gmail.com>
2023-04-23 12:57:07 -0300

    BaSP-W06/2023: Finished login page JS. Login functions check for email and password correct inputs

--- Week-06/JS/app-login.js
+++ Week-06/JS/app-login.js
@@ -1,4 +1,4 @@
-var emailInput = document.querySelector("#emailInput")
+var emailInput = document.querySelector("#emailInput");
 
 function blurEmailCheck(){
     var val = document.querySelector("#emailInput").value;
@@ -20,5 +20,52 @@ function focusEmailCheck(){
 
 emailInput.addEventListener("blur", blurEmailCheck);
 
-
 emailInput.addEventListener("focus", focusEmailCheck);
+
+
+
+var passwordInput = document.getElementById("passwordInput");
+
+
+function blurPasswordCheck(){
+    var val = document.querySelector("#passwordInput").value;
+    var regex = /^[A-Za-z0-9]*$/;
+    if (!(regex.test(val))){
+        var alert = document.getElementById("alertPassword");
+        alert.style.visibility = "visible";
+        passwordInput.classList.add("redBorder");
+    } else {
+        return
+    }
+}
+
+function focusPasswordCheck(){
+    var alert = document.getElementById("alertPassword");
+    alert.style.visibility = "hidden";
+    passwordInput.classList.remove("redBorder");
+}
+
+passwordInput.addEventListener("blur", blurPasswordCheck);
+
+passwordInput.addEventListener("focus", focusPasswordCheck);
+
+
+var button = document.getElementById("button");
+
+function loginClick (){
+    var emailText = document.querySelector("#emailInput").value;
+    var regexEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    var passwordText = document.getElementById("passwordInput").value;
+    var regexPassword = /^[A-Za-z0-9]*$/;
+    if ((regexEmail.test(emailText))&&(regexPassword.test(passwordText)&&(passwordText !== ""))){
+        alert("Successfully loged in with "+emailText+" as email and "+passwordText+" as password.");
+    } else if (((passwordText === "")||(!(regexPassword.test(passwordText))))&&((regexEmail.test(emailText))&&(!(emailText === "")))) {
+        alert("Please, enter a valid password.");
+    } else if (((passwordText !== "")&&(regexPassword.test(passwordText)))&&(!(regexEmail.test(emailText))||(emailText === ""))){
+        alert ("Please, enter a valid email.");
+    } else if(((passwordText === "")||(!(regexPassword.test(passwordText))))&&(!(regexEmail.test(emailText))||(emailText === ""))){
+        alert("Please enter a valid email and password");
+    }
+}
+
+button.addEventListener("click", loginClick);
